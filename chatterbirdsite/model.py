a7a8503108e4b252c9e1762b9d3abd5d1ea2adcf
Christopher Foo <chris.foo@gmail.com>
2017-01-12 13:01:33 -0500

    fixup! model: add tpp-waning-moon

--- chatterbirdsite/model.py
+++ chatterbirdsite/model.py
@@ -6,7 +6,7 @@ import os
 class AppModel(object):
     RUNS = [
         {
-            'slug': 'tpp-waning-monn',
+            'slug': 'tpp-waning-moon',
             'title': 'TwitchPlaysPokemon Waning Moon',
             'run_start': datetime.datetime(2017, 1, 13, 18, 0, tzinfo=datetime.timezone.utc),
             'url': 'http://www.twitch.tv/twitchplayspokemon'
