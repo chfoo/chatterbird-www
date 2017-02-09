f45dfeada994221ad862fae9c36fa58c0a9145ce
Christopher Foo <chris.foo@gmail.com>
2017-02-09 15:31:27 -0500

    mode: Add tpp-ani-2017

--- chatterbirdsite/model.py
+++ chatterbirdsite/model.py
@@ -6,6 +6,12 @@ import os
 class AppModel(object):
     RUNS = [
         {
+            'slug': 'tpp-ani-2017',
+            'title': 'TwitchPlaysPokemon 2017 Anniversary',
+            'run_start': datetime.datetime(2017, 2, 12, 21, 0, tzinfo=datetime.timezone.utc),
+            'url': 'http://www.twitch.tv/twitchplayspokemon'
+        },
+        {
             'slug': 'tpp-post-waning-moon',
             'title': 'TwitchPlaysPokemon Post Waning Moon',
             'run_start': datetime.datetime(2017, 1, 27, 6, 0, tzinfo=datetime.timezone.utc),
