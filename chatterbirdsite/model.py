9c61e24ab84d358256c4ea4d133a9d1c0c3de52e
Christopher Foo <chris.foo@gmail.com>
2017-08-10 14:17:46 -0400

    model: Add tpp-pyrite

--- chatterbirdsite/model.py
+++ chatterbirdsite/model.py
@@ -6,6 +6,12 @@ import os
 class AppModel(object):
     RUNS = [
         {
+            'slug': 'tpp-pyrite',
+            'title': 'TwitchPlaysPokemon Pyrite',
+            'run_start': datetime.datetime(2017, 8, 12, 21, 0, tzinfo=datetime.timezone.utc),
+            'url': 'http://www.twitch.tv/twitchplayspokemon'
+        },
+        {
             'slug': 'tpp-post-rand-white-2',
             'title': 'TwitchPlaysPokemon Post Randomized White 2',
             'run_start': datetime.datetime(2017, 6, 21, 19, 0, tzinfo=datetime.timezone.utc),
